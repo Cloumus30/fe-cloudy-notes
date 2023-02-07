578632eb728317610045896830e6cd7da975d173
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-02-07 15:27:59 +0700

    Add Flow bite and dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import { initDropdowns } from 'flowbite'
+import { onMounted } from 'vue';
+
+onMounted(()=>{
+  initDropdowns();
+})
 </script>
 
 <template>
   <header>
       <Navbar />
+
   </header>
 
   <RouterView />
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
+<script lang="ts">
   import {RouterLink} from 'vue-router'
   import IconBell from './icons/navbar/IconBell.vue';
   import IconUser from './icons/navbar/IconUser.vue';
   import IconLogin from './icons/navbar/IconLogin.vue';
   import IconDoc from './icons/navbar/IconDoc.vue';
+  import NotifCard from './partial/notifCard.vue';
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    components:{
+      IconBell, IconUser, IconLogin, IconDoc, RouterLink, NotifCard
+    },
+    data(){
+      return {
+        bodyNotif:"Notif Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate sit quis soluta ipsam est earum voluptates hic provident numquam excepturi.",
+        isNotifClicked: false
+      }
+    },
+    
+    methods:{
+      notifClick(value:boolean){
+        this.isNotifClicked = value;
+        console.log(this.isNotifClicked);
+      }
+    }
+  })  
 </script>
 
 <template>
@@ -12,11 +33,24 @@
           <p>Cloudy Notes</p>
         </div>
         <div class="flex mr-8">
-          <button class="mx-3 hover:text-white">
-            <IconBell/>
-          </button>
           <div class="group">
-            <button class="mx-3 border hover:text-white border-white rounded-full">
+            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="mx-3 hover:text-white">
+              <IconBell/>
+            </button>
+            <ul id="dropdown" class=" hidden absolute mt-4 bg-slate-800 drop-shadow rounded-md text-slate-300 right-10 w-1/3" aria-labelledby="dropdownDefaultButton">
+              <li class="py-4 pr-4 pl-2 flex hover:text-white hover:cursor-pointer"> 
+                 <NotifCard :body-notif="bodyNotif"/>
+              </li>
+              <li class="py-4 pr-4 pl-2 flex hover:text-white hover:cursor-pointer"> 
+                <NotifCard :body-notif="bodyNotif" />
+              </li>
+            </ul>
+          </div>
+          
+
+          <!-- Dropdown Profile -->
+          <div class="group">
+            <button class=" mx-3 border hover:text-white border-white rounded-full">
               <IconUser />
             </button>
             <ul class="absolute hidden pt-2 bg-slate-800 text-slate-300 right-2 group-hover:block hover:block">
@@ -28,6 +62,7 @@
               </li>
             </ul>
           </div>
+          <!-- DropDown Profile -->
           
           <!-- <RouterLink class="p-2" to="/about">About</RouterLink> -->
         </div>

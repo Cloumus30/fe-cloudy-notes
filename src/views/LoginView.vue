e190c5303abff378543e1427363496e820f17544
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-02-15 16:23:53 +0700

    Add Register page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -34,7 +34,15 @@ export default defineComponent({
                     <label for="checkbox-1" class="ml-2 text-sm font-medium text-font-navbar">Ingat saya</label>
                 </div>
                 
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-mono rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <div class="flex justify-around">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-mono rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Daftar</button>
+
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-mono rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login Google</button>
+                </div>
+
+                <div class="relative z-0 w-full mt-6">
+                    <p class="ml-2 text-sm font-medium text-font-navbar">Belum Punya Akun? Silahkan <a href="/register" class="text-white hover:underline"> Daftar </a></p>
+                </div>
             
                 
             </form>

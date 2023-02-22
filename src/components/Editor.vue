5d86ec338a42f28122fdb5e3b0357afe920bc46b
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-02-22 15:44:08 +0700

    Add Note QUill VUe

---
+++ src/components/Editor.vue
@@ -0,0 +1,85 @@
+<script lang="ts">
+import { Delta, QuillEditor } from '@vueup/vue-quill';
+import { defineComponent } from 'vue';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import '@vueup/vue-quill/dist/vue-quill.bubble.css';
+import ImageUploader from 'quill-image-uploader';
+import BlotFormatter from 'quill-blot-formatter';
+import HtmlDiffer from 'html-differ';
+
+export default defineComponent({
+    components:{
+        QuillEditor
+    },
+
+    data: () => {
+        return {
+           quillDat: "",
+        }
+    },
+
+    setup: ()=>{
+        const modules = [
+            {
+                name: 'imageUploader',
+                module: ImageUploader,
+                options:{
+                    upload: (file:any) => {
+                        return new Promise((resolve, reject) => {
+                            const formData = new FormData();
+                            formData.append("image", file);
+
+                            console.log(formData);
+
+                            setTimeout(() => {
+                                resolve(
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png"
+                                );
+                            }, 3500);
+                        })
+                    }
+                }, 
+            },
+            {
+                name: 'blotFormatter',
+                module: BlotFormatter,
+                options:{
+                }
+            }
+        ]
+        return {modules};
+    },
+    watch:{
+        // quillDat(current:any, old:any){
+        //     console.log(current.match(/<img([\w\W]+?)>/g));
+        // }
+    },
+    methods:{
+        handleClick() {
+            this.getImageSrcs();
+        },
+        getImageSrcs(){
+            const image = this.quillDat.match(/<img([\w\W]+?)>/g);
+            let srcs = [];
+            image?.forEach((el)=>{
+                const src =  el.match(/<img.*?src=['"](.*?)['"]/);
+                if(src){
+                    srcs.push(src[1])
+                }
+            });
+            return srcs;
+        },
+    }
+})
+
+</script>
+
+
+<template>
+    <QuillEditor theme="snow" class="h-5/6" toolbar="full" 
+        :modules="modules" 
+        v-model:content="quillDat" 
+        content-type="html"
+         />
+    <button @click="handleClick()">Klik Dinisi</button>
+</template>
--- src/components/Navside.vue
+++ src/components/Navside.vue
@@ -1,11 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
 import IconHome from '@/components/icons/navside/IconHome.vue';
 import IconDoc from '@/components/icons/navside/IconDocument.vue';
 
 export default defineComponent({
     components:{
-        IconHome, IconDoc
+        IconHome, IconDoc, RouterLink
     }
 })
 
@@ -15,7 +16,10 @@ export default defineComponent({
     <nav class="flex flex-row text-font-navbar bg-slate-900 h-screen">
         <ul class="m-6">
             <li class="mb-6">
-                <a class="hover:text-white flex flex-row" href=""> <IconHome class="mr-7 text-sm" /> Dashboard</a>
+                <!-- <a  href=""> <IconHome class="mr-7 text-sm" /> Dashboard</a> -->
+                <RouterLink class="hover:text-white flex flex-row" to="/login">  
+                    <IconHome class="mr-7 text-sm" /> Dashboard
+                </RouterLink>
             </li>
             <li class="mb-6">
                 <a class="hover:text-white flex flex-row" href=""> <IconDoc class="mr-7 text-sm" /> List Notes</a>

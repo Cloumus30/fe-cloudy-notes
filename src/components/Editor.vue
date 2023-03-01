4c76fe5ebf8a4bba2f945b24c36fc25da9ba8c01
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-03-01 11:05:08 +0700

    Update Editor Vue

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -14,6 +14,25 @@ export default defineComponent({
     data: () => {
         return {
            quillDat: "",
+           toolbars:[
+                        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+                        ['blockquote', 'code-block'],
+
+                        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+                        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+                        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+                        [{ 'direction': 'rtl' }],                         // text direction
+
+                        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+                        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+                        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+                        [{ 'font': [] }],
+                        [{ 'align': [] }],
+
+                        ['clean']                                         // remove formatting button
+                    ],
         }
     },
 
@@ -75,7 +94,7 @@ export default defineComponent({
 
 
 <template>
-    <QuillEditor theme="snow" class="h-5/6" toolbar="full" 
+    <QuillEditor theme="snow" class="h-5/6" :toolbar="toolbars" 
         :modules="modules" 
         v-model:content="quillDat" 
         content-type="html"
